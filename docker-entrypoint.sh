ff80f6b450445afbcc4eeecb88ab3bfa3f297796
Saul Shanabrook <s.shanabrook@gmail.com>
2016-01-13 17:38:08 -0500

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ echo "dind$RANDOM" > /usr/docker-compose-project-name
 
 function clean_up {
   docker rm -fv $(docker ps -a --format "{{.Names}}" | grep $(cat /usr/docker-compose-project-name)) || true
+  sleep 30m
 }
 
 trap clean_up EXIT
