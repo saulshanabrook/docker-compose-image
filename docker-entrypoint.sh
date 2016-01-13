f58de4648a7236e82c2f209246f0ae88dff42ee7
Saul Shanabrook <s.shanabrook@gmail.com>
2016-01-13 14:31:46 -0500

    use bash random for compose project name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,10 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+echo "dind$RANDOM" > /usr/docker-compose-project-name
+
 function clean_up {
-  docker rm -fv $(docker ps -a --format "{{.Names}}" | grep ${HOST})
+  docker rm -fv $(docker ps -a --format "{{.Names}}" | grep $(cat /usr/docker-compose-project-name))
 }
 
 trap clean_up EXIT
