f091b1c3ac2d33eb3f129e513fd3821b08999340
Saul Shanabrook <s.shanabrook@gmail.com>
2016-01-14 09:12:08 -0500

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -o xtrace
 export COMPOSE_PROJECT_NAME="dind$(cat /proc/sys/kernel/random/uuid  | sed 's/-//g')"
 
 function clean_up {
-  sleep 1hr
+  sleep 1h
   docker rm -fv $(docker ps -a --format "{{.Names}}" | grep $COMPOSE_PROJECT_NAME) || true
 }
 
