00544977c7b37b277c784b94b96894e1ff5efe5e
Saul Shanabrook <s.shanabrook@gmail.com>
2016-01-14 08:47:09 -0500

    use uuid for random

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-export COMPOSE_PROJECT_NAME="dind$RANDOM"
+export COMPOSE_PROJECT_NAME="dind$(cat /proc/sys/kernel/random/uuid  | sed 's/-//g')"
 
 function clean_up {
   docker rm -fv $(docker ps -a --format "{{.Names}}" | grep $COMPOSE_PROJECT_NAME) || true
